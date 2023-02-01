55fb98861d59113e96f183eb38287f9243e32c73
dmitrylawyer96 <104163560+dmitrylawyer96@users.noreply.github.com>
2023-02-01 14:01:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+/* №1 
+Задача на повернення ініціалів для кожного імені з масиву, 
+посортованих в алфавітному порядку:
+*/
+
+const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+let initials;
+
+// тут ваш код ...
+
+initials = userNames.sort();
+
+
+console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+
+
+
+// № 2 Задача на розворот числа:
+
+const currentMaxValue = 4589;
+let reverseMaxValue;
+
+// тут ваш код...
+
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'
+
+
+
+// № 3 Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:
+
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray;
+
+// тут ваш код...
+
+
+
+console.log(productOfArray); // 24
